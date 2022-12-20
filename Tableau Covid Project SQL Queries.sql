05f6a2311b6ade891e13bebd71486a4de02a268d
nishant-attri <120474992+nishant-attri@users.noreply.github.com>
2022-12-20 16:54:59 +0530

    Create Tableau Covid Project SQL Queries.sql

---
+++ Tableau Covid Project SQL Queries.sql
@@ -0,0 +1,87 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From xlcovid.CovidDeaths
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
+- 2. 
+
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From xlcovid.CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From xlcovid.CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From xlcovid.CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+--Other ideas for visualization
+
+
+--5.
+
+
+Select dea.continent, dea.location, dea.date, dea.population
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+From xlcovid.CovidDeaths dea
+Join xlcovid.CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population
+order by 1,2,3
+
+
+
+
+-- 6.
+
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From xlcovid.CovidDeaths dea
+Join xlcovid.CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
+From PopvsVac
+
+
+
+
+
+
+
